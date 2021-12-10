850536121e45630a4971633dd5c43f8f46b8c16b
umeshpandeyfaizabad <umeshpandey210@gmail.com>
2021-12-10 21:43:21 +0530

    App development started

---
+++ app/src/main/java/com/example/waie/model/PlanetaryData.kt
@@ -0,0 +1,3 @@
+package com.example.waie.model
+
+data class PlanetaryData(var date : String="", var url: String="", var title: String="", var explanation: String="")
---
+++ app/src/main/java/com/example/waie/utils/ProjectConstant.kt
@@ -0,0 +1,4 @@
+package com.example.waie.utils
+
+class ProjectConstant {
+}
